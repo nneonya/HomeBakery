2338467e1a2223cf4a8520751a46704681d3b3ac
nneonya <nneonya@mail.ru>
2024-05-15 03:56:20 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,56 @@
 window.onload = () => {
     const input = document.querySelector('.theme-switch-input');
+    
+    if (localStorage.getItem('theme') === 'dark') {
+        input.checked = true;
+        setDarkTheme();
+    } else {
+        input.checked = false;
+        setLightTheme();
+    }
+
     input.onchange = (e) => {
         if (input.checked) {
-            document.querySelectorAll('img').forEach(element => {
+            setDarkTheme();
+            localStorage.setItem('theme', 'dark');
+        } else {
+            setLightTheme();
+            localStorage.setItem('theme', 'light');
+        }
+    }
+
+    function setDarkTheme() {
+        document.querySelectorAll('img').forEach(element => {
+            if (element.src.includes('_light')) {
                 console.log(element.src);
                 element.src = element.src.replace('_light', '');
                 console.log(element.src);
-            })
-            setProp('--text', '#201E1C');
-            setProp('--background', '#E5D2BC');
-            setProp('--border', '2px solid #201E1C')
-        } else {
-            document.querySelectorAll('img').forEach(element => {
+            }
+        })
+        setProp('--text', '#201E1C');
+        setProp('--background', '#E5D2BC');
+        setProp('--border', '2px solid #201E1C');
+    }
+
+    function setLightTheme() {
+        document.querySelectorAll('img').forEach(element => {
+            if (!element.src.includes('_light')) {
                 console.log(element.src);
                 element.src = element.src.replace('.svg', '_light.svg');
                 console.log(element.src);
-            })
-            setProp('--text', '#E5D2BC');
-            setProp('--background', '#201E1C');
-            setProp('--border', '2px solid #E5D2BC')
-        }
+            }
+        })
+        setProp('--text', '#E5D2BC');
+        setProp('--background', '#201E1C');
+        setProp('--border', '2px solid #E5D2BC');
+    }
+
+    function setProp(property, value) {
+        document.documentElement.style.setProperty(property, value);
     }
 }
+
+
 
 function setProp(propName, value) {
     document.documentElement.style.setProperty(propName, value);
